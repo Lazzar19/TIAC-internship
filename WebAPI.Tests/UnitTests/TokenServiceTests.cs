133e177db9927d30d5ead1cfc5be7dcd651a9604
Lazzar17 <183487721+Lazzar19@users.noreply.github.com>
2026-08-03 19:54:10 +0200

    new tests for TokenServiceTests

--- WebAPI.Tests/UnitTests/TokenServiceTests.cs
+++ WebAPI.Tests/UnitTests/TokenServiceTests.cs
@@ -63,8 +63,79 @@ public class TokenServiceTests
         refreshToken.Should().NotBeNullOrWhiteSpace();
     }
 
-    [Fact]
+     [Fact]
     public void GenerateRefreshToken_Should_Return_Different_Values_On_Subsequent_Calls()
+    {
+        var firstToken = _tokenService.GenerateRefreshToken();
+        var secondToken = _tokenService.GenerateRefreshToken();
+
+        firstToken.Should().NotBe(secondToken);
+    }
+
+    [Fact]
+    public void GenerateToken_Should_Contain_Role_Claim()
+    {
+        var newUser = new User { ID = 1, Username = "testuser", Email = "test@test.com", PasswordHash = "hash", Role = Role.Admin };
+        var token = _tokenService.GenerateToken(newUser);
+
+        var handler = new JwtSecurityTokenHandler();
+        var jwt = handler.ReadJwtToken(token);
+
+        jwt.Claims.Should().Contain(c => 
+            c.Type == System.Security.Claims.ClaimTypes.Role && 
+            c.Value == Role.Admin.ToString());
+    }
+
+    [Fact]
+    public void GenerateToken_Should_Contain_UniqueName_Claim()
+    {
+        var username = "johndoe";
+        var newUser = new User{ ID = 2, Username = username, Email = "john@test.com", PasswordHash = "hash" };
+        var token = _tokenService.GenerateToken(newUser);
+
+        var handler = new JwtSecurityTokenHandler();
+        var jwt = handler.ReadJwtToken(token);
+
+        jwt.Claims.Should().Contain(c => 
+            c.Type == JwtRegisteredClaimNames.UniqueName && 
+            c.Value == username);
+    }
+    
+
+    [Fact]
+    public void GenerateToken_Should_Use_Configured_Audience()
+    {
+        var newUser = new User { ID = 4, Username = "testuser", Email = "test@test.com", PasswordHash = "hash" };
+        var token = _tokenService.GenerateToken(newUser);
+
+        var handler = new JwtSecurityTokenHandler();
+        var jwt = handler.ReadJwtToken(token);
+
+        jwt.Audiences.Should().Contain("TestAudience");
+    }
+
+    [Fact]
+    public void GenerateRefreshToken_Should_Return_Base64String()
+    {
+        var refreshToken = _tokenService.GenerateRefreshToken();
+
+        Action act = () => Convert.FromBase64String(refreshToken);
+        act.Should().NotThrow();
+        refreshToken.Should().NotBeNullOrWhiteSpace();
+    }
+
+    [Fact]
+    public void GenerateRefreshToken_Should_Return_64_Bytes()
+    {
+        var refreshToken = _tokenService.GenerateRefreshToken();
+
+        var bytes = Convert.FromBase64String(refreshToken);
+
+        bytes.Should().HaveCount(64);
+    }
+
+    [Fact]
+    public void GenerateRefreshToken_Should_Generate_Different_Values()
     {
         var firstToken = _tokenService.GenerateRefreshToken();
         var secondToken = _tokenService.GenerateRefreshToken();
