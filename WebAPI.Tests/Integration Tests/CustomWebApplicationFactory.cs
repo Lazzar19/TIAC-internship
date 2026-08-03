655d1ef07e3b35739ec19ab37170bc39d4bdbef2
Lazzar17 <183487721+Lazzar19@users.noreply.github.com>
2026-08-03 20:29:16 +0200

    new integration tests for ProductController

---
+++ WebAPI.Tests/Integration Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebAPI.Tests.Integration_Tests;
+
+using FluentAssertions;
+using Xunit;
+
+using WebAPI.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+
+        builder.ConfigureServices(services =>
+        {
+            var descriptor =
+                services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("IntegrationTestDB");
+            });
+
+            var sp = services.BuildServiceProvider();
+            using (var scope = sp.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                dbContext.Database.EnsureCreated();
+            }
+
+        });
+
+
+    }
+    
+        
+    
+    
+}
---
+++ WebAPI.Tests/Integration Tests/ProductControllerIntegrationTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Net;
+
+namespace WebAPI.Tests.Integration_Tests;
+using FluentAssertions;
+using Xunit;
+using WebAPI.Domain;
+
+
+
+public class ProductControllerIntegrationTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _client;
+    private readonly CustomWebApplicationFactory _factory;
+
+    public ProductControllerIntegrationTests(CustomWebApplicationFactory factory)
+    {
+        _factory = factory;
+        _client = factory.CreateClient();
+    }
+
+
+    [Fact]
+
+    public async Task Get_All_Products_Should_Return_Ok()
+    {
+        var response = await _client.GetAsync("/api/Product");
+        response.StatusCode.Should().Be(HttpStatusCode.Accepted);
+    }
+
+    [Fact]
+    public async Task Create_Product_With_Valid_Data_Should_Return_Created()
+    {
+        var createProductDTO = new CreatedProductDTO
+        {
+            Name = "test",
+            Description = "test description",
+            Price = 100.10m,
+            Stock = 10
+        };
+
+        var content = new StringContent(System.Text.Json.JsonSerializer.Serialize((createProductDTO)),
+            System.Text.Encoding.UTF8, "application/json");
+        
+        var response = await _client.PostAsync("/api/Product", content);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+    }
+
+    [Fact]
+    public async Task Create_Product_With_Invalid_Data_Should_Return_BadRequest()
+    {
+        var createProductDTO = new CreatedProductDTO
+        {
+            Name = "",
+            Description = "",
+            Price = -15,
+            Stock = -10
+        };
+        
+        var content = new StringContent(
+            System.Text.Json.JsonSerializer.Serialize(createProductDTO),
+            System.Text.Encoding.UTF8,
+            "application/json");;
+
+        var response = await _client.PostAsync("/api/Product", content);
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+    
+
+
+}
